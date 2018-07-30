f7e3ae18de437accde210f94862a1acab3a02a32
zhangkeke-nihao <2543206458@qq.com>
2018-07-30 19:21:56 +0800

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,26 +20,28 @@ export default class Header extends Component{
         return (
             <div className="header-wrap">
                <Avatar size="large" src={require('../../image/icon.png')} style={{width:"100px",height:"100px"}}/>
-               <div className="head">三班人</div>
-               <div className="header-content-left">
+            <div>               
+                <div className="head">三班人</div>
+                <div className="header-content-left">
                     {this.renderHeadMsg()}
-               </div>
-               <div className="header-content-right">
-                    <div>
-                        <span className="num">手机号码：<span>1311238756</span></span>
-                        <Icon style={{fontSize:'16px'}} type="file" />
-                    </div>
-                    <div>
-                        <span>微信号码：</span>
-                        <Input style={{width:'100px'}} size="small" placeholder="" />
-                        <Button style={{width:'30px',height:'25px',fontSize:'12px',padding:'0',color:'#000'}}>提交</Button>
-                    </div>
-                    <div>
-                        <span className="remark">备注：</span>
-                        <Input style={{width:'100px'}} size="small" placeholder="" />
-                        <Button style={{width:'30px',height:'25px',fontSize:'12px',padding:'0',color:'#000'}}>提交</Button>
-                    </div>
-               </div>
+                </div>
+            </div>
+            <div className="header-content-right">
+                <div>
+                    <span className="num">手机号码：<span>1311238756</span></span>
+                    <Icon style={{fontSize:'16px'}} type="file" />
+                </div>
+                <div>
+                    <span>微信号码：</span>
+                    <Input style={{width:'100px'}} size="small" placeholder="" />
+                    <Button style={{width:'30px',height:'25px',fontSize:'12px',padding:'0',color:'#000'}}>提交</Button>
+                </div>
+                <div>
+                    <span className="remark">备注：</span>
+                    <Input style={{width:'100px'}} size="small" placeholder="" />
+                    <Button style={{width:'30px',height:'25px',fontSize:'12px',padding:'0',color:'#000'}}>提交</Button>
+                </div>
+           </div>
             </div>
         )
     }
