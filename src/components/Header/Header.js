6d554d57e0be6808aba97a4f88973a9d2b50edb7
zhangkeke-nihao <2543206458@qq.com>
2018-07-30 19:26:36 +0800

    7月30日作业

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,13 +20,13 @@ export default class Header extends Component{
         return (
             <div className="header-wrap">
                <Avatar size="large" src={require('../../image/icon.png')} style={{width:"100px",height:"100px"}}/>
-                <div>               
+               <div>
                     <span className="head">三班人</span>
                     <div className="header-content-left">
-                        {this.renderHeadMsg()}
+                            {this.renderHeadMsg()}
                     </div>
-                </div>
-                <div className="header-content-right">
+               </div>
+               <div className="header-content-right">
                     <div>
                         <span className="num">手机号码：<span>1311238756</span></span>
                         <Icon style={{fontSize:'16px'}} type="file" />
@@ -42,7 +42,7 @@ export default class Header extends Component{
                         <Button style={{width:'30px',height:'25px',fontSize:'12px',padding:'0',color:'#000'}}>提交</Button>
                     </div>
                </div>
-           </div>
+            </div>
         )
     }
 }
