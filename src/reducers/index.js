a4acfaa44df83b3d1adc215f384cd14d4459b6fd
zhangkeke-nihao <2543206458@qq.com>
2018-08-01 20:13:21 +0800

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import userMessage from '../reducers/userMessage'
 import tableMessage from '../reducers/tableMessage'
-import tableColums from '../reducers/tableColums'
 
 export default combineReducers({
   userMessage,
   tableMessage,
-  tableColums
 });
 
 
