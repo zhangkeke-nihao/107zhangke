7ba68f7003bfb5d1ff1db2569a1209df3f2f60a9
zhangkeke-nihao <18730818961@163.com>
2018-08-25 14:50:53 +0800

    '2048'

--- src/components/game-explain/explian.js
+++ src/components/game-explain/explian.js
@@ -11,7 +11,7 @@ export default class Explian extends Component {
       <div className="game_info">
         <div className="game_info_left">
           <div className="game_explain">Keypressing W S A D on PC.</div>
-          <div className="game_explain">Touch moving on Phone! Join it &amp; have fun!</div>
+          <div className="game_explain">Touch moving on Phone! <br />Join it &amp; have fun!</div>
         </div>
         <div className="game_restart" onClick={() => this.props.Actions.restartGameNum(0)}>
           <span>Restart</span>
--- src/components/game-score/gamescore.js
+++ src/components/game-score/gamescore.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 
 import './gamescore.css';
 
+let scoreanimate1 = false;
+let scoreanimate2 = false;
 export default class GameScore extends Component {
   state = { }
 
+  renderScore = () => {
+    const { scoreAdd } = this.props;
+    if (scoreAdd !== 0) {
+      scoreanimate1 = !scoreanimate1;
+      return (
+        <div className={`score-addition_${scoreanimate1}`}>+ {scoreAdd}</div>
+      );
+    }
+    return null;
+  }
+  renderbestScore = () => {
+    const { score, bestScore, bestScoreAdd } = this.props;
+    if (bestScore <= score && bestScoreAdd !== 0) {
+      scoreanimate2 = !scoreanimate2;
+      return (
+        <div className={`score-addition_${scoreanimate2}`}>+ {bestScoreAdd}</div>
+      );
+    }
+    return null;
+  }
   render() {
     const {
       score, bestScore
@@ -15,9 +37,11 @@ export default class GameScore extends Component {
         <div className="score_container">
           <div className="scores">
             {score}
+            {this.renderScore()}
           </div>
           <div className="bestScore">
             {bestScore}
+            {this.renderbestScore()}
           </div>
         </div>
       </div>
